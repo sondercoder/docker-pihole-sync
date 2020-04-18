a345c3ca728a34fa9bb2312fa4aab4b26e6c11d0
sondercoder <56107276+sondercoder@users.noreply.github.com>
2020-04-18 01:36:17 -0700

    Update syncScript.sh
    Requires elevated privs

--- syncScript.sh
+++ syncScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 inotifywait -r -m -e close_write --format '%w%f' /mnt/pihole /mnt/dnsmasq.d/ | while read MODFILE
 do
-    bash -c "rsync -aP /mnt/pihole/ $CLIENTDIR1 --delete"
-    bash -c "rsync -aP /mnt/dnsmasq.d/ $CLIENTDIR2 --delete"
+    sudo bash -c "rsync -aP /mnt/pihole/ $CLIENTDIR1 --delete"
+    sudo bash -c "rsync -aP /mnt/dnsmasq.d/ $CLIENTDIR2 --delete"
 done
