090e9408c2c3d9048fd6dbaeec10bec1f05f19c0
sondercoder <56107276+sondercoder@users.noreply.github.com>
2020-04-18 01:04:30 -0700

    Update syncScript.sh
    Added dnsmasq.d path to be monitored and synced as well.

--- syncScript.sh
+++ syncScript.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-inotifywait -r -m -e close_write --format '%w%f' /mnt/pihole | while read MODFILE
+inotifywait -r -m -e close_write --format '%w%f' /mnt/pihole /mnt/dnsmasq.d/ | while read MODFILE
 do
-    bash -c "rsync -aP /mnt/pihole/ $CLIENTDIR --delete"
+    bash -c "rsync -aP /mnt/pihole/ $CLIENTDIR1 --delete"
+    bash -c "rsync -aP /mnt/dnsmasq.d/ $CLIENTDIR2 --delete"
 done
