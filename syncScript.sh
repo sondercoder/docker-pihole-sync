4dafb45138b80881c82b60e1a84c35ef3d456b00
sondercoder <56107276+sondercoder@users.noreply.github.com>
2020-04-20 15:51:09 -0700

    Update syncScript.sh
    exclude log db, as copying the master db to the slave mean's you'd lose logs from the slave, also prevents extra writes on a microsd card if you are using a raspberry pi
    
    info: https://docs.pi-hole.net/ftldns/database/

--- syncScript.sh
+++ syncScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 inotifywait -r -m -e close_write --format '%w%f' /mnt/pihole /mnt/dnsmasq.d/ | while read MODFILE
 do
-    sudo bash -c "rsync -aP /mnt/pihole/ $CLIENTDIR1 --delete"
+    sudo bash -c "rsync --exclude 'pihole-FTL.db' /mnt/pihole/ $CLIENTDIR1 --delete"
     sudo bash -c "rsync -aP /mnt/dnsmasq.d/ $CLIENTDIR2 --delete"
 done
