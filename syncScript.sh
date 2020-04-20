a71058f284212383a81375ba913031da19775234
sondercoder <56107276+sondercoder@users.noreply.github.com>
2020-04-20 16:09:02 -0700

    Update syncScript.sh

--- syncScript.sh
+++ syncScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-inotifywait -r -m -e close_write --format '%w%f' /mnt/pihole /mnt/dnsmasq.d/ | while read MODFILE
+inotifywait -r -m -e close_write --exclude 'pihole-FTL.db' --format '%w%f' /mnt/pihole /mnt/dnsmasq.d/ | while read MODFILE
 do
     sudo bash -c "rsync -aP --exclude 'pihole-FTL.db' /mnt/pihole/ $CLIENTDIR1 --delete"
     sudo bash -c "rsync -aP /mnt/dnsmasq.d/ $CLIENTDIR2 --delete"
