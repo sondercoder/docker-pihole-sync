cae2497740ded72ad138b92c023d0b7c0e22703d
sondercoder <56107276+sondercoder@users.noreply.github.com>
2025-05-20 20:13:10 -0700

    Update syncScript.sh
    excluded additional files that shouldn't be replicated.

--- syncScript.sh
+++ syncScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-inotifywait -r -m -e close_write --exclude 'pihole-FTL.db' --format '%w%f' /mnt/pihole /mnt/dnsmasq.d/ | while read MODFILE
+inotifywait -r -m -e close_write --exclude 'pihole-FTL.db*' --format '%w%f' /mnt/pihole /mnt/dnsmasq.d/ | while read MODFILE
 do
-    sudo bash -c "rsync -aP --exclude 'pihole-FTL.db' /mnt/pihole/ $CLIENTDIR1 --delete"
+    sudo bash -c "rsync -aP --exclude pihole-FTL.db* --exclude gravity*.* --exclude ./listsCache/list* /mnt/pihole/ $CLIENTDIR1 --delete"
     sudo bash -c "rsync -aP /mnt/dnsmasq.d/ $CLIENTDIR2 --delete"
 done
